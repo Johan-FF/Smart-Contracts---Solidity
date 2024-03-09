c5e38e6b5c10f6aa15fabb1f808d2fe0c22ec3fd
Johan F. <johan.artmat@gmail.com>
2024-03-09 11:13:59 -0500

    [UPDATE] Deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const ima = await ethers.deployContract("Ima", []);
+  const ima = await ethers.deployContract("Ima", [10000]);
 
   await ima.waitForDeployment();
 
